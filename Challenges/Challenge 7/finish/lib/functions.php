8ec996f68b41d7ddd9837cbc77bc470a3b57be47
WarpedNet <102426484+WarpedNet@users.noreply.github.com>
2024-03-05 18:31:51 +0000

    Challenge 7

---
+++ Challenges/Challenge 7/finish/lib/functions.php
@@ -0,0 +1,46 @@
+<?php
+
+function get_connection()
+{
+    $config = require 'config.php';
+
+    $pdo = new PDO(
+        $config['database_dsn'],
+        $config['database_user'],
+        $config['database_pass']
+    );
+
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    return $pdo;
+}
+
+function get_pets($limit = null)
+{
+    $pdo = new PDO(
+        $config['database_dsn'],
+        $config['database_user'],
+        $config['database_pass']
+    );
+    $result = $pdo->query('SELECT * FROM pet');
+    $rows = $result->fetchAll();
+
+    return $pets;
+}
+
+function save_pets($petsToSave)
+{
+    $json = json_encode($petsToSave, JSON_PRETTY_PRINT);
+    file_put_contents('data/pets.json', $json);
+}
+
+function get_pet($id)
+{
+    $pdo = get_connection();
+    $query = 'SELECT * FROM pet WHERE id = :idVal';
+    $stmt = $pdo->prepare($query);
+    $stmt->bindParam('idVal', $id);
+    $stmt->execute();
+
+    return $stmt->fetch();
+}
